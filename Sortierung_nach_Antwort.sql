2605922b6794bbd3fa51fdf7da235a87402a366a
TibiLM <96882649+TibiLM@users.noreply.github.com>
2021-12-30 17:59:42 +0100

    Add files via upload

---
+++ Sortierung_nach_Antwort.sql
@@ -0,0 +1,13 @@
+SELECT t.IDPerson1, t.IDPerson2, SUM(t.Score) AS Score
+FROM (
+
+		SELECT Pa1.ID_Person AS IDPerson1, Pa1.ID_Frage, Pa1.Antwort AS Antwort1, Pa2.ID_Person AS IDPerson2, 
+			Pa2.Antwort AS Antwort2,
+			(CASE WHEN Pa1.Antwort = Pa2.Antwort THEN 1 ELSE 0 END) AS Score 
+		FROM T_Person_Antwort Pa1
+			INNER JOIN T_Person_Antwort Pa2 ON Pa1.ID_Frage = Pa2.ID_Frage
+		WHERE Pa1.ID_Person = 1 AND Pa2.ID_Person <> 1
+		--ORDER BY  Pa2.ID_Person, Pa2.ID_Frage
+		) t
+GROUP BY t.IDPerson1, t.IDPerson2
+ORDER BY t.IDPerson1, t.IDPerson2, Score
